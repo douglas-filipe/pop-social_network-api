8967635b4b75da847dca63d15539f3e80cc78383
Douglas Filipe <douglasfelipe.net77@gmail.com>
2022-01-15 21:36:43 -0400

    fix: picture url user

--- src/models/User.js
+++ src/models/User.js
@@ -1,41 +1,37 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    picture: { type: String, required: false },
   },
   { timestamps: true }
 );
 
 //Rodar antes dos dados
-userSchema.pre('save', function(next){
-  if(this.isNew || this.idModified('password')){
-    bcrypt.hash(this.password, 10,
-      (err, hashedPassword) => {
-        if(err)
-          next(err)
-        else{
-          this.password = hashedPassword
-          next()
-        }
-      }  
-      
-    )
+userSchema.pre("save", function (next) {
+  if (this.isNew || this.idModified("password")) {
+    bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+      if (err) next(err);
+      else {
+        this.password = hashedPassword;
+        next();
+      }
+    });
   }
-})
+});
 
-
-userSchema.methods.isCorrectPassword = function(password, callback){
-  bcrypt.compare(password, this.password, function(err, same){
-    if(err){
-      callback(err)
-    }else{
-      callback(err, same)
+userSchema.methods.isCorrectPassword = function (password, callback) {
+  bcrypt.compare(password, this.password, function (err, same) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, same);
     }
-  })
-}
+  });
+};
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model("User", userSchema);
